0396470735b2711801b1c3315496f9b0b2e52767
yzhao24 <165528874+yzhao24@users.noreply.github.com>
2024-04-08 00:28:03 +0000

    completed version

--- app.rb
+++ app.rb
@@ -2,8 +2,45 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+
+  erb(:homepage)
+
 end
+
+
+get("/rock") do
+  moves = ["rock", "paper", "scissors"]
+
+  @comp_move = moves.sample
+
+  if @comp_move == "rock"
+    @outcome = "tied"
+  elsif @comp_move =="paper"  
+    @outcome = "lost"
+  else
+    @outcome = "won"  
+  end
+
+  erb(:zebra)
+end
+
+get("/paper") do
+
+  moves = ["rock", "paper", "scissors"]
+
+  @comp_move = moves.sample
+
+  erb(:giraffe)
+
+end  
+
+
+get("/scissors") do
+
+  moves = ["rock", "paper", "scissors"]
+
+  @comp_move = moves.sample
+
+  erb(:cow)
+
+end  
